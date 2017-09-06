108bb3843e20ee84c0d422dfd639c587f4d7daa0
Klimonov <dh-ka@bk.ru>
2017-09-06 18:24:16 +0300

    fix console error

--- assets/js/template.js
+++ assets/js/template.js
@@ -452,16 +452,7 @@ jQuery(window).on('scroll', function () {
 
 
 
-			$.ajax({
-			type: "POST",
-			url: "assets/php/mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			$(this).find("input").val("");
-			$(this).find("select").val("");
-			$(this).find("textarea").val("");
-			$(".form").trigger("reset");
-		});
+
 
 
 
